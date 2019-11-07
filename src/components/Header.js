6b1536c898bce36fe03d99a4ae9a4df6565ce6d1
Heather Nuffer <zzzuzz@gmail.com>
2019-11-07 13:15:10 -0700

    Created and added first styled-component element

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,23 @@
 import React from "react";
+import styled from "styled-components";
 
 function Header() {
+
+  const TopHeader = styled.div`
+    margin: 0 auto 3rem auto;
+    background: black;
+    color: white;
+    padding: 2rem;
+  `;
+
   return (
-    <div className="header top-header">
+    <TopHeader >
         <img src="https://www.nasa.gov/sites/all/themes/custom/nasatwo/images/nasa-logo.svg" alt="NASA logo" />
       <h1>
         NASA <span role="img" aria-label="rocket">🚀</span> APOD 
       </h1>
       <h2>Astronomy Picture of the Day</h2>
-    </div>
+    </TopHeader>
   );
 }
 
